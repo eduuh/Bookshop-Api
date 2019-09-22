5881751d8fb62887d03950962de98f70e380d98a
edwin muraya <edwinkamaumuraya0@outlook.com>
2019-09-22 05:41:47 -0700

    create a IDesignTimeDbContextFactory for the dbcontext

--- booksApi/Services/BookDbContextFactory.cs
+++ booksApi/Services/BookDbContextFactory.cs
@@ -5,17 +5,29 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.Extensions.Configuration;
+using System.IO;
 
 namespace booksApi.Services
 {
     public class BookDbContextFactory : IDesignTimeDbContextFactory<BookDbContext>
     {
-       
-       
+
+        private static IConfiguration configuration { get; set; }
+        
+        public BookDbContextFactory()
+        {
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true);
+             
+        }
         public BookDbContext CreateDbContext(string[] args)
         {
+            var  connectionstring = configuration.GetConnectionString("bookDbConnectionString");
             var optionbuilder = new DbContextOptionsBuilder<BookDbContext>();
-            var connectionstring = "Server=(localdb)\\mssqllocaldb;Database=BookApiProject;Trusted_Connection=True;";
+            //var connectionstring = "Server=(localdb)\\mssqllocaldb;Database=BookApiProject;Trusted_Connection=True;"; 
+            
+
 
             optionbuilder.UseSqlServer(connectionstring);
             return new BookDbContext(optionbuilder.Options);
