de2324a13e9be32e6013e64c4187f5129e2a590e
edwin muraya <edwinkamaumuraya0@outlook.com>
2019-09-22 05:19:51 -0700

    created a dbcontext class for a project Defined all the dbset properties in the dbcontext class which will match the database tables created a connections string in the application.json file which points to localdb database. Override the OnModelCreating and inside the method used the Modelbuilder to define the primary and foreign keys of the many to many relationships added the dbcontext as service in our mvc pipeline

---
+++ booksApi/Services/BookDbContextFactory.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
+
+namespace booksApi.Services
+{
+    public class BookDbContextFactory : IDesignTimeDbContextFactory<BookDbContext>
+    {
+       
+       
+        public BookDbContext CreateDbContext(string[] args)
+        {
+            var optionbuilder = new DbContextOptionsBuilder<BookDbContext>();
+            var connectionstring = "Server=(localdb)\\mssqllocaldb;Database=BookApiProject;Trusted_Connection=True;";
+
+            optionbuilder.UseSqlServer(connectionstring);
+            return new BookDbContext(optionbuilder.Options);
+        }
+    }
+}
