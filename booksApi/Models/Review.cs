aea3d50b905d82b0d12e01eb426bba3857c6f346
edwin muraya <edwinkamaumuraya0@outlook.com>
2019-09-22 02:29:35 -0700

    created the entity classes => Country, Category, Review , Book , Author created the one to many relatioship between classes           author and country(has many authors)           review and reviewer(can write many reviews)           review and book (has many reviews)
    created the Many to many  relatioship between Book author.
             book and author
             book and categories
    added the data annotations for the Entity classes => Contry ,category ,Review , Reviewer ,Book, Author

---
+++ booksApi/Models/Review.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace booksApi.Models
+{
+    public class Review
+    {
+        public int Id { get; set; }
+        public string HeadLine { get; set; }
+        public string ReviewText { get; set; }
+        public int Ratings { get; set; }
+    }
+}
---
+++ booksApi/Models/Reviewer.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace booksApi.Models
+{
+    public class Reviewer
+    {
+        public int Id { get; set; }
+        public string Firstname { get; set; }
+        public string Lastname { get; set; }
+
+    }
+}
