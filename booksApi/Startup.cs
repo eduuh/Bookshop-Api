83b4aea220b8ccb1e1d3ce211f16daea52c160e4
edwin muraya <edwinkamaumuraya0@outlook.com>
2019-10-26 07:53:21 +0300

    Solve my bug for resource not found

--- booksApi/Startup.cs
+++ booksApi/Startup.cs
@@ -8,39 +8,39 @@ using Microsoft.EntityFrameworkCore;
 
 namespace booksApi
 {
-    public class Startup
+  public class Startup
+  {
+    public static IConfiguration configuration { get; set; }
+    public Startup(IConfiguration configurations)
     {
-        public static IConfiguration Configuration { get; set; }
-        public Startup(IConfiguration configuration)
-        {
-            Configuration = configuration;
-        }
-        // This method gets called by the runtime. Use this method to add services to the container.
-        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
-        public static void ConfigureServices(IServiceCollection services)
-        {
-            services.AddMvc();
-
-            //var connectionstring = Configuration["ConnectionStrings:bookDbConnectionString"];
-
-            services.AddDbContext<BookDbContext>(options =>
-                options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
-
-            services.AddScoped<ICountryRepository, CountryRepository>();
-        }
-
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public static void Configure(IApplicationBuilder app, IHostingEnvironment env,BookDbContext context)
-        {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
-
-            context.SeedDataContext();
-            app.UseMvcWithDefaultRoute();
-            app.UseMvc();
-
-        }
+      configuration = configurations;
     }
+    // This method gets called by the runtime. Use this method to add services to the container.
+    // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+    public static void ConfigureServices(IServiceCollection services)
+    {
+      services.AddMvc();
+
+      //var connectionstring = Configuration["ConnectionStrings:bookDbConnectionString"];
+      var DefaultConnection = "Server=(localdb)\\MSSQLLocalDB;Database=booksdb;Trusted_Connection=True;MultipleActiveResultSets=true;";
+      services.AddDbContext<BookDbContext>(options =>
+                options.UseSqlServer(DefaultConnection));
+
+      services.AddScoped<ICountryRepository, CountryRepository>();
+    }
+
+    // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+    public static void Configure(IApplicationBuilder app, IHostingEnvironment env, BookDbContext context)
+    {
+      if (env.IsDevelopment())
+      {
+        app.UseDeveloperExceptionPage();
+      }
+
+      context.SeedDataContext();
+      app.UseMvcWithDefaultRoute();
+      app.UseMvc();
+
+    }
+  }
 }
