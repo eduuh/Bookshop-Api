9341139bbfc83fe4481cc7fad8efc07e082b37b7
edwin muraya <edwinkamaumuraya0@outlook.com>
2019-10-26 12:51:23 +0300

    Removed the database project

--- BookApiSqlDatabase/dbo/Tables/Books.sql
+++
@@ -1,8 +0,0 @@
-﻿CREATE TABLE [dbo].[Books] (
-    [Id]            INT            IDENTITY (1, 1) NOT NULL,
-    [isbn]          NVARCHAR (10)  NOT NULL,
-    [Title]         NVARCHAR (200) NOT NULL,
-    [DatePublished] DATETIME2 (7)  NULL,
-    CONSTRAINT [PK_Books] PRIMARY KEY CLUSTERED ([Id] ASC)
-);
-
